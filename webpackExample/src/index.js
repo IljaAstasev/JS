1ee2d847f3eaf292810b81a0f3123a9f6f039acf
sergeyts <sergeysill@gmail.com>
2021-09-22 12:54:44 +0300

    Features 5

--- webpackExample/src/index.js
+++ webpackExample/src/index.js
@@ -1,1 +1,16 @@
-index.js
+const newModul = require('./main');
+
+const newModulInstanse = new newModul();
+
+newModulInstanse.hello();
+newModulInstanse.goodbye();
+
+// Браузер ничего не знает о наших ипортах и require. Нужен сборщик
+/*
+
+npm init -y
+npm i webpack webpack- cli --save-dev
+npx-webpacka
+
+
+*/
